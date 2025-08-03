04364c87ea36446b4ea3bcd41f6b8dcc54d2fc0d
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-08-03 20:50:56 +0530

    Add support for sets, sorted sets, and hashes in Redis data structures

--- data-structures.js
+++ data-structures.js
@@ -24,9 +24,61 @@ const redisDataStructures = async() => {
         console.log(email, age, country);
 
         //lists -> LPUSH, RPUSH, LRANGE, LPOP, RPOP
-        await client.lPush('notes', ['note 1', 'note 2', 'note 3']);
+        await client.lPush('notes', ['note 1', 'note 2', 'note 3']); //comment to avoid performing push operation in each successive run 
         const extractAllNotes = await client.lRange('notes', 0, -1);
         console.log(extractAllNotes);
+        
+        const firstNote = await client.lPop('notes');
+        console.log(firstNote);
+
+        //sets -> SADD, SMEMBERS, SISMEMBER, SREM (remove)
+        await client.sAdd('user:nickName', ['john', 'varun', 'xyz']); 
+        const extractNickNames = await client.sMembers('user:nickName');
+        console.log(extractNickNames);
+        const isVarunOneOfNickName = await client.sIsMember('user:nickName', 'varun');
+        console.log(isVarunOneOfNickName);
+        await client.sRem('user:nickName', 'xyz'); 
+
+        //sorted sets -> ZADD, ZRANGE, ZRANK, ZREM
+        await client.zAdd('cart', [
+            {
+                score: 100, 
+                value: 'Cart 1'
+            },
+            {
+                score: 150, 
+                value: 'Cart 2'
+            },
+            {
+                score: 10, 
+                value: 'Cart 3'
+            }
+        ]);
+        const getTopCartItems = await client.zRange('cart', 0, -1);
+        console.log(getTopCartItems);
+
+        const getCartItemsWithScore = await client.zRangeWithScores('cart', 0, -1);
+        console.log(getCartItemsWithScore);
+
+        const cartTwoRank = await client.zRank('cart', 'Cart 2'); 
+        console.log(cartTwoRank);
+
+        //hashes -> HSET, HGET, HGETALL. HDEL
+        await client.hSet('product:1', {
+            name: 'Product 1', 
+            decription: 'product one description', 
+            rating: '5'
+        });
+
+        const getProductRating = await client.hGet('product:1', 'rating'); 
+        console.log(getProductRating);
+
+        const getProductDetails = await client.hGetAll('product:1');
+        console.log(getProductDetails);
+
+        await client.hDel('product:1', 'rating'); 
+        const updatedProductDetails = await client.hGetAll('product:1'); 
+        console.log(updatedProductDetails);
 
     } catch (error) {
         console.log(error)
