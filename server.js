5ac0cb4811ecb162441c12a8e322733d8912ccd1
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-08-03 19:20:41 +0530

    Implement Redis client connection and error handling

---
+++ server.js
@@ -0,0 +1,25 @@
+const redis = require('redis'); 
+
+//create client
+const client = redis.createClient({
+    host: 'localhost',
+    port: 6379
+});
+
+//event listener for redis client error
+client.on('erorr', (err) => {
+    console.log('Redis client error occured!', err)
+});
+
+const testRedisConnection = async() => {
+    try {
+        await client.connect();
+        console.log('Redis connection successful!');
+    } catch (error) {
+        console.log(error);
+    } finally { //to make sure there is no open connection
+        client.quit();
+    }
+};
+
+testRedisConnection();
